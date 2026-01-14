a87c1bb8236c0779c984e45f7d1a71db5515a184
OLSZA <144938068+0LSZA@users.noreply.github.com>
2026-01-14 21:28:36 +0100

    Dodanie ekranu końcowego, wypisywanie w konsoli postaci, słownik zapisujący postacie

--- Gra_detektywistyczna/Assets/MenuScripts/MenuControl.cs
+++ Gra_detektywistyczna/Assets/MenuScripts/MenuControl.cs
@@ -8,13 +8,15 @@ using System;
 
 public class MenuControl : MonoBehaviour
 {
+    public static Dictionary<string, string> CollectedCharacters = new Dictionary<string, string>();
+
     public static MenuControl Instance;
     public static int CurrentSceneNumber { get; private set; }
     public static string CurrentScenarioName { get; private set; }
 
     [Header("UI References")]
     [SerializeField] private Image backgroundImage;
-    
+
     private async void Awake()
     {
         await DialogueEngineManager.InitializeManagerAsync(gameObject);
@@ -40,42 +42,70 @@ public class MenuControl : MonoBehaviour
 
     public async void NewGameScene()
     {
-        string[] parameters = { "scenerio_apollo", "1"};
-            
+        string[] parameters = { "scenerio_apollo", "1" };
+
         CurrentScenarioName = parameters[0];
         CurrentSceneNumber = int.Parse(parameters[1]);
 
+
         SceneScriptDTO scene = await DialogueEngineManager.Instance.GetSceneAsync(parameters);
-        SceneManager.LoadScene("NewGame");
+
+        if (scene != null)
+        {
+
+            MenuControl.CollectedCharacters.Clear();
+
+
+            foreach (var character in scene.Npcs)
+            {
+                if (!MenuControl.CollectedCharacters.ContainsKey(character.name))
+                {
+                    MenuControl.CollectedCharacters.Add(character.name, character.protrait);
+                    Debug.Log($"Dodano do słownika: {character.name} ze ścieżką {character.protrait}");
+                }
+            }
+
+
+            SceneManager.LoadScene("NewGame");
+        }
+
     }
 
     public async void NextScene()
     {
         string[] parameters = { CurrentScenarioName, (++CurrentSceneNumber).ToString() };
         SceneScriptDTO scene = await DialogueEngineManager.Instance.GetSceneAsync(parameters);
+
+
         if (scene != null)
         {
+
             Debug.Log("Nazwa tla to: " + scene.Background);
 
-            // Wywołujemy funkcję zmiany tła
+
             SetBackground(scene.Background);
 
-            Debug.Log("Next scene");
-            
+            Debug.Log("Next scene: " + CurrentSceneNumber);
+        }
+        else
+        {
+            Debug.Log("Wykryto koniec scenariusza (null). Zmieniam scenę na podsumowanie.");
+
+            UnityEngine.SceneManagement.SceneManager.LoadScene("EndGameSummary");
         }
     }
+
     private void SetBackground(string backgroundName)
     {
-        // 1. Ładujemy obrazek z folderu Resources
+
         Sprite newSprite = Resources.Load<Sprite>(backgroundName);
-        if (backgroundImage == null) 
-{
-    return;
-}
+        if (backgroundImage == null)
+        {
+            return;
+        }
 
         if (newSprite != null)
         {
-            // 2. Podmieniamy Source Image w komponencie Image
             backgroundImage.sprite = newSprite;
         }
         else
@@ -88,7 +118,7 @@ public class MenuControl : MonoBehaviour
         SceneManager.LoadScene("ContinueGame");
     }
 
-    public void  AuthorsScene()
+    public void AuthorsScene()
     {
         SceneManager.LoadScene("Authors");
     }
@@ -107,5 +137,5 @@ public class MenuControl : MonoBehaviour
 #endif
 
     }
-    
+
 }
---
+++ Gra_detektywistyczna/Assets/MenuScripts/SummaryManager.cs
@@ -0,0 +1,82 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System.Collections.Generic;
+
+public class SummaryManager : MonoBehaviour
+{
+    [Header("Konfiguracja UI")]
+    public RectTransform canvasRect;
+    public GameObject cardTemplate;
+
+    void Start()
+    {
+        if (MenuControl.CollectedCharacters == null || MenuControl.CollectedCharacters.Count == 0)
+        {
+            Debug.LogWarning("SummaryManager: Brak zebranych postaci.");
+            return;
+        }
+        DisplayCharacters();
+    }
+
+    private void DisplayCharacters()
+    {
+        var characters = MenuControl.CollectedCharacters;
+        float totalWidth = canvasRect.rect.width;
+        float spacing = totalWidth / (characters.Count + 1);
+
+        int i = 1;
+        foreach (var entry in characters)
+        {
+            GameObject newCard = Instantiate(cardTemplate, canvasRect);
+            newCard.SetActive(true);
+            newCard.transform.SetAsLastSibling();
+
+            RectTransform rt = newCard.GetComponent<RectTransform>();
+            float posX = (-totalWidth / 2f) + (spacing * i);
+            rt.anchoredPosition = new Vector2(posX, 0);
+
+            TextMeshProUGUI txt = newCard.GetComponentInChildren<TextMeshProUGUI>();
+            if (txt != null)
+            {
+                txt.text = entry.Key;
+                txt.raycastTarget = false;
+            }
+
+            Image img = newCard.GetComponent<Image>();
+            if (img != null)
+            {
+                img.raycastTarget = false;
+
+                string path = !string.IsNullOrEmpty(entry.Value) ? entry.Value : entry.Key;
+                Sprite s = Resources.Load<Sprite>(path);
+                if (s != null) img.sprite = s;
+                else Debug.LogError($"[SummaryManager] Brak pliku '{path}' w Resources!");
+            }
+
+            Button btn = newCard.GetComponentInChildren<Button>();
+            if (btn != null)
+            {
+                string charName = entry.Key;
+                btn.onClick.RemoveAllListeners();
+                btn.onClick.AddListener(() => OnCardClicked(charName));
+
+                Image btnImg = btn.GetComponent<Image>();
+                if (btnImg != null) btnImg.raycastTarget = true;
+            }
+            else
+            {
+                Debug.LogError($"[SummaryManager] Nie znaleziono przycisku wewn?trz karty {entry.Key}!");
+            }
+
+            i++;
+        }
+        cardTemplate.SetActive(false);
+    }
+
+    public void OnCardClicked(string name)
+    {
+        Debug.Log("Klikniêta postaæ: " + name);
+    }
+
+}
